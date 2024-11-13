1d536dd73b360e1975bbca3760f622c811d9cc41
GlenCarvajalHidalgo <ggdegleng@gmail.com>
2024-11-13 15:14:37 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
 document.querySelector('.cabecera-boton').addEventListener('click', function(){
     document.querySelector('.cabecera-navegacion2').classList.toggle('active');
+    document.querySelector('.cabecera-boton').classList.toggle('active');
 });
